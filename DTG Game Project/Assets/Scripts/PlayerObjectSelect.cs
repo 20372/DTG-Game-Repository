586cf3ddb8878b355690e6d471a437e40148072f
20372 <160555383+20372@users.noreply.github.com>
2024-06-26 18:23:34 +1200

    Testing Computer Hacking System

--- DTG Game Project/Assets/Scripts/PlayerObjectSelect.cs
+++ DTG Game Project/Assets/Scripts/PlayerObjectSelect.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerObjectSelect : MonoBehaviour
 {
     RaycastHit hit;
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -20,6 +21,10 @@ public class PlayerObjectSelect : MonoBehaviour
         if (Physics.Raycast(cameraRay, out hit))
         {
             Debug.Log(hit.collider.gameObject.name + " was hit");
+            if(hit.collider.gameObject.CompareTag("Table"))
+            {
+                SceneManager.LoadScene("Computer", LoadSceneMode.Additive);
+            }
            
         }
     }
